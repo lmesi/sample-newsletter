d042a6ff26746b3d6a0b4abc0f94d7bae79ba0ca
Emese Laszlo <lszl.emese@gmail.com>
2018-07-24 21:25:35 +0200

    Initial commit
    Initialize Spring Boot Project

---
+++ src/main/java/org/marketing/samplenewsletter/SampleNewsletterApplication.java
@@ -0,0 +1,12 @@
+package org.marketing.samplenewsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleNewsletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleNewsletterApplication.class, args);
+	}
+}
---
+++ src/test/java/org/marketing/samplenewsletter/SampleNewsletterApplicationTests.java
@@ -0,0 +1,16 @@
+package org.marketing.samplenewsletter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleNewsletterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
